6417136a5764fe3a15815c4b0224457516b2a102
Youngjae Choi <cdw0807@naver.com>
2022-06-17 15:57:55 +0900

    Initial Commit

---
+++ MemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoApp
+//
+//  Created by 최영재 on 2022/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by 최영재 on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
